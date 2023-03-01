b29a53c98f763a76be3171fa94ef236715096ca1
jfoyarzo <foyarzo621@gmail.com>
2023-03-01 03:21:31 -0300

    Add hero partial and set up layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  layout :select_layout
   before_action :authenticate_user!
+
+  private
+
+  def select_layout
+    @current_user.present? ? 'application' : 'no_hero'
+  end
 end
