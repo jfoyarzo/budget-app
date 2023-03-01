b9f2b3f147d8b420deb6f6f44a44973222f5c218
jfoyarzo <foyarzo621@gmail.com>
2023-03-01 04:00:03 -0300

    Add actions to groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group, only: :show
+
+  def index
+    @groups = current_user.groups
+  end
+
+  def show; end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
