80869e378a86732617af49693e1a7ca947fc69f4
jfoyarzo <foyarzo621@gmail.com>
2023-03-02 20:34:14 -0300

    Add multiple categories checkbox and validation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-  before_action :set_group, only: %i[index create]
+  before_action :set_group
 
   def index
     @payments = @group.payments
@@ -7,18 +7,24 @@ class PaymentsController < ApplicationController
 
   def new
     @payment = Payment.new
+    @groups = current_user.groups
   end
 
   def create
-    @payment = Payment.new(payment_params)
-    @payment.author = current_user
+    if params[:payment][:groups].nil?
+      flash[:alert] = 'Must select at least one category'
+      redirect_to new_group_payment_path(@group)
+    else
+      @payment = Payment.new(payment_params)
+      @payment.author = current_user
 
-    respond_to do |f|
-      if @payment.save
-        GroupPayment.create(payment: @payment, group: @group)
-        f.html { redirect_to group_payments_path(@group.id), notice: 'The Payment was added successfully.' }
-      else
-        f.html { render :index, status: :unprocessable_entity }
+      respond_to do |f|
+        if @payment.save
+          params[:payment][:groups].each { |group| GroupPayment.create(payment: @payment, group_id: group) }
+          f.html { redirect_to group_payments_path(@group.id), notice: 'The Payment was added successfully.' }
+        else
+          f.html { render :index, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -30,6 +36,6 @@ class PaymentsController < ApplicationController
   end
 
   def payment_params
-    params.require(:payment).permit(:name, :amount)
+    params.require(:payment).permit(:name, :amount, categories: [])
   end
 end
